0e3eca11173c24fd20345583cb2b7affff2d7107
feiquanbifeng <460429192@qq.com>
2014-01-21 00:23:15 +0800

    Create excercise_struct_args.go

---
+++ excercise_struct_args.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+  "fmt"
+)
+
+type Person struct {
+  GetName func(n string) string
+}
+
+func (self Person) Say() {
+	fmt.Println("say person")
+}
+
+func Hello(n string) string {
+	return n
+}
+
+func main() {
+	p := &Person {Hello}
+	ret := p.GetName("good")
+	fmt.Println(ret)
+	p.Say()
+}

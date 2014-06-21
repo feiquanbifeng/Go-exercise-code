d211616eca5a3680f90529bab836d227cdeeae40
feiquanbifeng <460429192@qq.com>
2014-06-21 08:43:14 +0800

    Update exercise_command.go

--- exercise_command.go
+++ exercise_command.go
@@ -1,44 +1,44 @@
-    package main
+package main
 
-    import (
-        "fmt"
-    )
+import (
+    "fmt"
+)
 
-    // command interface
-    type Commander interface {
-        execute()
-    }
+// command interface
+type Commander interface {
+    execute()
+}
 
-    // who receive the command
-    type Recever struct {
-    }
+// who receive the command
+type Recever struct {
+}
 
-    // receiver do something when the command is received
-    func (r *Recever) action() {
-        fmt.Println("the Recever action has been token!")
-    }
+// receiver do something when the command is received
+func (r *Recever) action() {
+    fmt.Println("the Recever action has been token!")
+}
 
-    type Invoker struct {
-        Commander
-    }
+type Invoker struct {
+    Commander
+}
 
-    func (i *Invoker) action() {
-        i.Commander.execute()
-    }
+func (i *Invoker) action() {
+    i.Commander.execute()
+}
 
-    // the concrete command
-    type ConcreteCommand struct {
-        *Recever
-    }
+// the concrete command
+type ConcreteCommand struct {
+    *Recever
+}
 
-    func (c *ConcreteCommand) execute() {
-        c.Recever.action()
-    }
+func (c *ConcreteCommand) execute() {
+    c.Recever.action()
+}
 
-    func main() {
-        receiver := new(Recever) // &Receiver{}
-        command := &ConcreteCommand{receiver}
+func main() {
+    receiver := new(Recever) // &Receiver{}
+    command := &ConcreteCommand{receiver}
 
-        invoker := Invoker{command}
-        invoker.action()
-    }
+    invoker := Invoker{command}
+    invoker.action()
+}

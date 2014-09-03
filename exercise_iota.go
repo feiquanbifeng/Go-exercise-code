7eab8cd1970587ce62139abad9e2f7642b8a29d6
feiquanbifeng <460429192@qq.com>
2014-09-03 09:32:19 +0800

    Create exercise_iota.go

---
+++ exercise_iota.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	const (
+		A1 = iota
+		A2
+		str = "hello"
+		s
+		A3 = iota
+		A4
+	)
+	fmt.Println("A1=", A1, "A2=", A2, "s=", s, "str=", str, "A3=", A3, "A4=", A4)
+}

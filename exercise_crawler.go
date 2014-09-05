a44a7457214c6e6d147a96ea044bb9ba2452084f
feiquanbifeng <460429192@qq.com>
2014-09-05 16:41:43 +0800

    Update exercise_crawler.go

--- exercise_crawler.go
+++ exercise_crawler.go
@@ -5,10 +5,51 @@ package main
 import (
     "net/http"
     "io/ioutil"
+    "time"
     "fmt"
     "regexp"
     "strings"
+    _ "database/sql"
+
+    "log"
+    "gopkg.in/mgo.v2"
+    // "gopkg.in/mgo.v2/bson"
 )
+
+const (
+    URL = "127.0.0.1:27017"
+)
+
+type Article struct {
+    Title string
+    CreateTime time.Time
+}
+
+func save(title string) {
+    session, err := mgo.Dial(URL)
+        if err != nil {
+                panic(err)
+        }
+        defer session.Close()
+
+        // Optional. Switch the session to a monotonic behavior.
+        session.SetMode(mgo.Monotonic, true)
+
+        c := session.DB("crawler").C("article")
+        err = c.Insert(&Article{title, time.Now()})
+        if err != nil {
+                fmt.Println(err)
+                log.Fatal(err)
+        }
+
+        /*result := Person{}
+        err = c.Find(bson.M{"name": "Ale"}).One(&result)
+        if err != nil {
+                log.Fatal(err)
+        }
+
+        fmt.Println("Phone:", result.Phone)*/
+}
 
 func main() {
     client := &http.Client{}
@@ -25,6 +66,7 @@ func main() {
         matchs = r.FindAllStringSubmatch(bodystr, -1)
         for _, v := range matchs {
             fmt.Println(v[1])
+            save(v[1])
         }
     }
 }

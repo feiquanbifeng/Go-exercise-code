f25e4e058b111f8efb91bde69402845d57bea6a9
feiquanbifeng <460429192@qq.com>
2014-09-11 09:56:37 +0800

    Update exercise_synccond.go

--- exercise_synccond.go
+++ exercise_synccond.go
@@ -2,81 +2,81 @@ package main
 
 import (
     "bytes"
-	"log"
-	"sync"
+    "log"
+    "sync"
 )
 
 type Waiter struct {
-	expected      []byte
-	expectedMutex sync.Mutex
-	actual        []byte
-	done          bool
-	cond          *sync.Cond
+    expected      []byte
+    expectedMutex sync.Mutex
+    actual        []byte
+    done          bool
+    cond          *sync.Cond
 }
 
 func (w *Waiter) Write(p []byte) (int, error) {
-	w.expectedMutex.Lock()
-	defer w.expectedMutex.Unlock()
-	w.actual = append(w.actual, p...)
-	if bytes.Contains(w.actual, w.expected) {
-		w.cond.L.Lock()
-		defer w.cond.L.Unlock()
-		w.done = true
-		w.cond.Broadcast()
-	}
-	return len(p), nil
+    w.expectedMutex.Lock()
+    defer w.expectedMutex.Unlock()
+    w.actual = append(w.actual, p...)
+    if bytes.Contains(w.actual, w.expected) {
+        w.cond.L.Lock()
+        defer w.cond.L.Unlock()
+        w.done = true
+        w.cond.Broadcast()
+    }
+    return len(p), nil
 }
 
 func (w *Waiter) Wait() {
-	w.cond.L.Lock()
-	defer w.cond.L.Unlock()
-	for !w.done {
-		w.cond.Wait()
-	}
+    w.cond.L.Lock()
+    defer w.cond.L.Unlock()
+    for !w.done {
+        w.cond.Wait()
+    }
 }
 
 func New(expected []byte) *Waiter {
-	return &Waiter{
-		expected: expected,
-		cond:     sync.NewCond(&sync.Mutex{}),
-	}
+    return &Waiter{
+        expected: expected,
+        cond:     sync.NewCond(&sync.Mutex{}),
+    }
 }
 
 func main() {
-	const count = 10
-	expected := []byte("42")
-	w := New(expected)
+    const count = 10
+    expected := []byte("42")
+    w := New(expected)
 
-	event := make(chan bool)
-	for i := 0; i < count; i++ {
-		go func() {
-			if r := recover(); r != nil {
-				log.Fatal(r)
-			}
-			w.Write([]byte("4"))
-			w.Write([]byte("2"))
-			event <- true
-		}()
-	}
+    event := make(chan bool)
+    for i := 0; i < count; i++ {
+        go func() {
+            if r := recover(); r != nil {
+                log.Fatal(r)
+            }
+            w.Write([]byte("4"))
+            w.Write([]byte("2"))
+            event <- true
+        }()
+    }
 
-	for i := 0; i < count; i++ {
-		go func() {
-			if r := recover(); r != nil {
-				log.Fatal(r)
-			}
-			w.Wait()
-			event <- true
-		}()
-	}
+    for i := 0; i < count; i++ {
+        go func() {
+            if r := recover(); r != nil {
+                log.Fatal(r)
+            }
+            w.Wait()
+            event <- true
+        }()
+    }
 
-	done := make(chan struct{})
-	go func() {
-		defer close(done)
-		for i := 0; i < count*2; i++ {
-			<-event
-		}
-	}()
+    done := make(chan struct{})
+    go func() {
+        defer close(done)
+        for i := 0; i < count*2; i++ {
+            <-event
+        }
+    }()
 
-	<-done
-	w.Wait()
+    <-done
+    w.Wait()
 }
